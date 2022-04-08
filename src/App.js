1dcd83231d223c788d164b9c830d60cd8c6fd35b
minh chiu <minh.mchiu@gmail.com>
2022-04-08 09:13:26 +0700

    #3 Query error state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { pokeApi } from './api/pokeApi'
 function App() {
   const queryInfo = useQuery('pokemon', async () => {
     await new Promise(resolve => setTimeout(resolve, 1000))
+    if (true) {
+      throw new Error('Test error!')
+    }
     return pokeApi.getAll()
   })
   console.log({ queryInfo })
 
   if (queryInfo.isLoading) return <h2>Loading...</h2>
+  if (queryInfo.isError)
+    return <h2 style={{ color: 'red' }}>{queryInfo.error.message}</h2>
 
   return (
     <>
