23610491298588aadca736d576f1c0f622ca2af9
minh chiu <minh.mchiu@gmail.com>
2022-04-08 09:50:18 +0700

    #8 Using Custom Hooks to Share and Reuse Queries

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useQuery } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { pokeApi } from './api/pokeApi'
 
-const Pokemon = ({ queryKey }) => {
-  const queryInfo = useQuery(queryKey, async () => {
+const usePokemon = () =>
+  useQuery('pokemon', async () => {
     await new Promise(resolve => setTimeout(resolve, 1000))
     return pokeApi.getAll()
   })
 
+const Pokemon = () => {
+  const queryInfo = usePokemon()
   if (queryInfo.isLoading) return <h2>Loading...</h2>
   if (queryInfo.isError)
     return <h2 style={{ color: 'red' }}>{queryInfo.error.message}</h2>
@@ -21,13 +23,17 @@ const Pokemon = ({ queryKey }) => {
   )
 }
 
+const Count = () => {
+  const queryInfo = usePokemon()
+  return <h3>You are looking at {queryInfo.data?.results.length} pokemons.</h3>
+}
+
 function App() {
   return (
     <>
       <h1>React query</h1>
-      {<Pokemon queryKey='pokemon1' />}
-      <br />
-      {<Pokemon queryKey='pokemon1' />}
+      <Count />
+      <Pokemon />
       <ReactQueryDevtools />
     </>
   )
